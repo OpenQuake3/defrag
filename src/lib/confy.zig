88820ee8609d889d611e3cb733c2060723565e31
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-23 19:19:18 +0200

    git: Update confy submodule

--- src/lib/confy.zig
+++ src/lib/confy.zig
@@ -1,2 +1,5 @@
+//:___________________________________________________________________
+//  osdf  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:___________________________________________________________________
 // @fileoverview Cable connector to the confy library
 pub usingnamespace @import("./confy/src/confy.zig");
