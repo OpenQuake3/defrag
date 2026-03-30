63921447d0b1f77129aa42935a59243c0308aca2
sOkam <7308253+heysokam@users.noreply.github.com>
2026-03-30 22:51:51 +0200

    bld: Add `build/cfg.zig` file

---
+++ src/build/cfg.zig
@@ -0,0 +1,11 @@
+//:_________________________________________________________________
+//  osdf  |  Copyright (C) Ivan Mar (sOkam!)  |  GPL-3.0-or-later  :
+//:_________________________________________________________________
+pub const cfg = @This();
+
+pub const modname = "osdf";
+
+pub const dir = struct {
+  pub const assets = "assets";
+};
+
